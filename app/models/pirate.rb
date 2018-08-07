ec588922dd7778db3aaf65580beb3422797b3554
Adam Urbina <adamurbina@gmail.com>
2018-08-07 18:33:58 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,7 +4,10 @@ class Pirate
 
     attr_accessor :name, :weight, :height
 
-    def initialize
+    def initialize(name, weight, height)
+        @name = name
+        @weight = weight
+        @height = height
         @@all << self
     end
 
